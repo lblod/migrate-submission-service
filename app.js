4295962abe1e1817adb1cfd7be57268494886a7c
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-03-26 20:28:20 +0100

    added some ui-experience ;-)

--- app.js
+++ app.js
@@ -13,15 +13,12 @@ import { createDataBuckets } from './data-bucket-helpers';
 import { insertTtlFile } from './file-helpers';
 import { calculateMetaSnapshot } from './copied-code/enrich-submission-service/submission-document';
 
-app.get('/', (req, res) => {
-  res.send('Hello from migrate-submission-service');
-});
-
-app.get('/start-migration', async (req, res) => {
-  let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
+async function migrateFormsForFormNode(formNode){
+  const start = new Date();
   const inzendingen = await getInzendingVoorToezichtToDo(formNode);
 
   let count = 0;
+  let errorCount = 0;
   const total = inzendingen.length;
   for(let inzending of inzendingen){
     const task = await createTaskForMigration(inzending.inzendingUri);
@@ -38,10 +35,29 @@ app.get('/start-migration', async (req, res) => {
     catch(e){
       console.log(`Error for ${inzending.inzendingUri}`);
       console.error(e);
+      ++errorCount;
       await updateTask(task.taskUri, 1, FAILED);
     }
     ++count;
     console.log(`---------------- Processed ${count} of ${total} ----------------`);
   }
+
+  const end = new Date();
+
+  console.log(`Finished processing ${total} forms`);
+  console.log(`Started at ${start}`);
+  console.log(`Ended at ${end}`);
+  console.log(`Total errors ${errorCount}`);
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello from migrate-submission-service');
 });
+
+app.get('/start-migration', async (req, res) => {
+  let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
+  migrateFormsForFormNode(formNode);
+  res.send({msg: 'job started' });
+});
+
 app.use(errorHandler);
