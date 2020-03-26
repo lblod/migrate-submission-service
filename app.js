edeb1ae32787b28ff38c5eaa09352949fb9bc1b2
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-03-26 17:52:48 +0100

    some 'cleanup'

--- app.js
+++ app.js
@@ -5,64 +5,27 @@ import  { getInzendingVoorToezichtToDo,
           constructInzendingContentTtl,
           insertData,
           ONGOING,
+          FAILED,
           FINISHED
         } from './queries';
 
 import { createDataBuckets } from './data-bucket-helpers';
 import { insertTtlFile } from './file-helpers';
 import { calculateMetaSnapshot } from './copied-code/enrich-submission-service/submission-document';
-import besluitTypes from './besluit-types-list';
 
 app.get('/', (req, res) => {
   res.send('Hello from migrate-submission-service');
 });
 
-async function getOneInzendingPerType(){
-  let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
-  const bestuurseenheid = null;
-  const inzending = null;
-  const limit = 1;
-  let inzendingen =[];
-  for(let besluitType of besluitTypes){
-    const results = await getInzendingVoorToezichtToDo(formNode, bestuurseenheid, inzending, besluitType, limit);
-    inzendingen = [...inzendingen, ...results];
-  }
-  console.log(`Found ${inzendingen.length} for formNode ${formNode}`);
-  return inzendingen;
-}
-
-async function getInzendingen(){
-  //GET: InzendingVoorToezicht to do  (specifc rootNode)
-  //This is the latest <http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7>
-  let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
-  const bestuurseenheid = null; 'http://data.lblod.info/id/bestuurseenheden/974816591f269bb7d74aa1720922651529f3d3b2a787f5c60b73e5a0384950a4'; //gem Aalst
-  const inzending = null; 'http://data.lblod.info/inzendingen-voor-toezicht/5DF3942CA3ACB60008000420';
-  const besluitType = null; 'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03';
-  const limit = null;
-  const inzendingen = await getInzendingVoorToezichtToDo(formNode, bestuurseenheid, inzending, besluitType, limit);
-  console.log(`Found ${inzendingen.length} for formNode ${formNode}`);
-  return inzendingen;
-}
-
-async function getSpecificInzending(){
-let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
-  const bestuurseenheid = null; 'http://data.lblod.info/id/bestuurseenheden/974816591f269bb7d74aa1720922651529f3d3b2a787f5c60b73e5a0384950a4'; //gem Aalst
-  const inzending = 'http://data.lblod.info/inzendingen-voor-toezicht/5E67C1A2A3ACB600080003B7';
-  const besluitType = null; 'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03';
-  const limit = null;
-  const inzendingen = await getInzendingVoorToezichtToDo(formNode, bestuurseenheid, inzending, besluitType, limit);
-  console.log(`Found ${inzendingen.length} for formNode ${formNode}`);
-  return inzendingen;
-}
-
 app.get('/start-migration', async (req, res) => {
-
-  const inzendingen = await getSpecificInzending();
+  let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
+  const inzendingen = await getInzendingVoorToezichtToDo(formNode);
 
   let count = 0;
+  const total = inzendingen.length;
   for(let inzending of inzendingen){
+    const task = await createTaskForMigration(inzending.inzendingUri);
     try {
-      const task = await createTaskForMigration(inzending.inzendingUri);
       const inzendingTTl = await constructInzendingContentTtl(inzending.inzendingUri);
       const data = await createDataBuckets(inzendingTTl);
       await insertTtlFile(data.fileGraph.value, data.formTtlFile.value, data.turtleFormTtlContent, data.nTriplesFileGraph);
@@ -73,10 +36,12 @@ app.get('/start-migration', async (req, res) => {
       await updateTask(task.taskUri, 1, FINISHED);
     }
     catch(e){
-      debugger;
+      console.log(`Error for ${inzending.inzendingUri}`);
+      console.error(e);
+      await updateTask(task.taskUri, 1, FAILED);
     }
     ++count;
-    console.log(count);
+    console.log(`---------------- Processed ${count} of ${total} ----------------`);
   }
 });
 app.use(errorHandler);
---
+++ debug-helpers.js
@@ -0,0 +1,52 @@
+import besluitTypes from './besluit-types-list';
+import  { getInzendingVoorToezichtToDo } from './queries';
+/**
+ * Since this migration is all about testing, a wrote some functions which might be helpful for debugging purposes.
+ * You will however have to modify code in app.js to use them.
+ */
+
+/***********************************************************
+ * FILTER FUNCTIONS
+ ***********************************************************/
+async function getInzendingen(){
+  //This is the latest <http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7>
+  let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
+  const bestuurseenheid = null; // 'http://data.lblod.info/id/bestuurseenheden/974816591f269bb7d74aa1720922651529f3d3b2a787f5c60b73e5a0384950a4'; //gem Aalst
+  const inzending = null;  // 'http://data.lblod.info/inzendingen-voor-toezicht/5DF3942CA3ACB60008000420';
+  const besluitType = null; // x'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03';
+  const limit = null;
+  const inzendingen = await getInzendingVoorToezichtToDo(formNode, bestuurseenheid, inzending, besluitType, limit);
+  console.log(`Found ${inzendingen.length} for formNode ${formNode}`);
+  return inzendingen;
+}
+
+async function getSpecificInzending(){
+  let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
+  const bestuurseenheid = null; // 'http://data.lblod.info/id/bestuurseenheden/974816591f269bb7d74aa1720922651529f3d3b2a787f5c60b73e5a0384950a4'; //gem Aalst
+  const inzending = 'http://data.lblod.info/inzendingen-voor-toezicht/5E67C1A2A3ACB600080003B7';
+  const besluitType = null; // 'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03';
+  const limit = null;
+  const inzendingen = await getInzendingVoorToezichtToDo(formNode, bestuurseenheid, inzending, besluitType, limit);
+  console.log(`Found ${inzendingen.length} for formNode ${formNode}`);
+  return inzendingen;
+}
+
+async function getOneInzendingPerType(){
+  let formNode = 'http://data.lblod.info/form-nodes/77fa3d4b1310b08f49ca334ac13153a5953a9feba2c6bfb7c555dc9d45a1d1d7';
+  const bestuurseenheid = null;
+  const inzending = null;
+  const limit = 1;
+  let inzendingen =[];
+  for(let besluitType of besluitTypes){
+    const results = await getInzendingVoorToezichtToDo(formNode, bestuurseenheid, inzending, besluitType, limit);
+    inzendingen = [...inzendingen, ...results];
+  }
+  console.log(`Found ${inzendingen.length} for formNode ${formNode}`);
+  return inzendingen;
+}
+
+export {
+  getInzendingen,
+  getSpecificInzending,
+  getOneInzendingPerType
+}
